4b3764f1b7823d0ac70daf89f6e9f7cc73f8a980
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-11-07 03:24:28 +0500

    Fix clerkClient.users not found issue

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,26 +2,26 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
-export const getClerkUser = async ({ userIds }: { userIds: string[] }) => {
+
+export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
-    const clerk = clerkClient();
-    const { data } = await clerkClient.users.getUserList({
+    const apiData = (await clerkClient()).users.getUserList({
       emailAddress: userIds,
     });
 
-    const users = data.map((user: any) => ({
+    const users = (await apiData).data.map((user: any) => ({
       id: user.id,
       name: `${user.firstName} ${user.lastName}`,
       email: user.emailAddresses[0].emailAddress,
       avatar: user.imageUrl,
     }));
 
-    const sortedUsers = userIds.map((email) => {
-      users.find((user: any) => user.email == email);
-    });
+    const sortedUsers = userIds.map((email) =>
+      users.find((user: any) => user.email === email)
+    );
 
     return parseStringify(sortedUsers);
   } catch (error) {
-    console.log(`Error while getting user: ${error}`);
+    console.log(`Error fetching users: ${error}`);
   }
 };
